547a14729a084ab5ee7a260d3b64178d6f55ead6
Alberto Santagostino <santagostino.alb@gmail.com>
2021-12-06 13:59:01 +0100

    Refactor day02

--- src/day02.py
+++ src/day02.py
@@ -12,30 +12,23 @@ SUBMARINE_COMMANDS = {
 }
 
 
-def part1(puzzle_input):
-    position = array([0, 0])
-    for command in puzzle_input:
-        instruction, value = command.split()
-        action = SUBMARINE_COMMANDS[instruction]
-        position = position + (action * int(value))
-    return position[0] * position[1]
-
-
-def part2(puzzle_input):
+def solve(puzzle_input, consider_aim=False):
     position = array([0, 0])
     aim = 0
     for command in puzzle_input:
         instruction, value = command.split()
-        value = int(value)
         action = SUBMARINE_COMMANDS[instruction]
-        if instruction != 'forward':
-            aim = aim + (action * int(value))[1]
+        if not consider_aim:
+            position = position + (action * int(value))
         else:
-            position = position + array([value, value * aim])
+            if instruction != 'forward':
+                aim = aim + (action * int(value))[1]
+            else:
+                position = position + array([int(value), int(value) * aim])
     return position[0] * position[1]
 
 
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input(filename=__file__)
-    print(f"Part 1 solution: {part1(puzzle_input)}")
-    print(f"Part 2 solution: {part2(puzzle_input)}")
+    print(f"Part 1 solution: {solve(puzzle_input)}")
+    print(f"Part 2 solution: {solve(puzzle_input, consider_aim=True)}")
