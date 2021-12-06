a57bab13f4ab33982204b52a469d5481f7e11f78
Alberto Santagostino <santagostino.alb@gmail.com>
2021-12-06 13:35:03 +0100

    Add day05 part2 solution

--- src/day05.py
+++ src/day05.py
@@ -1,51 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import ipdb
-
 from collections import defaultdict
 
 from common.meta_utils import get_puzzle_input
 
 
-def part1(coordinates):
+def solve(coordinates, consider_diagonals=False):
     touched_coords = defaultdict(dict)
     for coordinate in coordinates:
         start, end = coordinate[0], coordinate[1]
         if start[0] == end[0]:
-            # Vertical line
             length = abs(start[1] - end[1]) + 1
             y_min = min(start[1], end[1])
             if not start[0] in touched_coords:
                 touched_coords[start[0]] = {}
             for y in range(y_min, y_min + length):
-                if not y in touched_coords[start[0]]:
-                    touched_coords[start[0]][y] = 1
-                else:
-                    touched_coords[
-                        start[0]][y] = touched_coords[start[0]][y] + 1
+                add_point(touched_coords, start[0], y)
         elif start[1] == end[1]:
-            # Horizontal line
             length = abs(start[0] - end[0]) + 1
             x_min = min(start[0], end[0])
             for x in range(x_min, x_min + length):
-                if not start[0] in touched_coords:
-                    touched_coords[start[0]] = {}
-                if not start[1] in touched_coords[x]:
-                    touched_coords[x][start[1]] = 1
-                else:
-                    touched_coords[x][
-                        start[1]] = touched_coords[x][start[1]] + 1
-
+                add_point(touched_coords, x, start[1])
+        elif consider_diagonals and start[0] != end[0] and start[1] != end[1]:
+            length = abs(start[1] - end[1]) + 1
+            if start[0] > end[0]:
+                start, end = switcharoo(start, end)
+            if start[1] <= end[1]:
+                for idx in range(0, length):
+                    add_point(touched_coords, start[0] + idx, start[1] + idx)
+            else:
+                for idx in range(0, length):
+                    add_point(touched_coords, start[0] + idx, start[1] - idx)
     count = 0
-    for kk, vv in touched_coords.items():
+    for vv in touched_coords.values():
         count = count + sum([1 for val in list(vv.values()) if val >= 2])
     return count
 
 
-def part2(coordinates):
-    #return res
-    ipdb.set_trace()
+def add_point(touched_coords, new_x, new_y):
+    if not new_y in touched_coords[new_x]:
+        touched_coords[new_x][new_y] = 1
+    else:
+        touched_coords[new_x][new_y] = touched_coords[new_x][new_y] + 1
 
 
 def parse_input(puzzle_input):
@@ -58,8 +55,14 @@ def parse_input(puzzle_input):
     return coordinates
 
 
+def switcharoo(el1, el2):
+    tmp = el1
+    el1 = el2
+    return el1, tmp
+
+
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input(filename=__file__)
     coordinates = parse_input(puzzle_input)
-    print(f"Part 1 solution: {part1(coordinates)}")
-    print(f"Part 2 solution: {part2(coordinates)}")
+    print(f"Part 1 solution: {solve(coordinates)}")
+    print(f"Part 2 solution: {solve(coordinates, consider_diagonals=True)}")
